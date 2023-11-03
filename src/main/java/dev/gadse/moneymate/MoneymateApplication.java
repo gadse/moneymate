b40bafdb74f144e5defd9c0cc5349b9a5c25da39
Peter Bozsoky <peter-bozsoky@posteo.de>
2023-11-03 12:10:10 +0100

    project: Initial structure
    Standard Spring Boot application and maven setup.

---
+++ src/main/java/dev/gadse/moneymate/MoneymateApplication.java
@@ -0,0 +1,13 @@
+package dev.gadse.moneymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gadse/moneymate/MoneymateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gadse.moneymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
